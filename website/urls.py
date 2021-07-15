08fe57e3ad272b13d9d8eda331e4cae31b72761b
Alex Park <jihoonhotmail.com@gmail.com>
2021-07-15 22:07:30 +0900

    home page html (home.html) hello page html (index.html)
    Functions in views.py (home, index)
    functions redirect to appropriate urls
    
    urls.py (imports added)
    paths added for home(homepage) and index(\hello)

--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+
+from hello.views import *
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
+    path('hello/', index),
 ]
