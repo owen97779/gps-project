16a52b2b7ce2c47ee7da9bc052d0b54f109b8854
Alex Park <jihoonhotmail.com@gmail.com>
2021-07-19 14:55:47 +0900

    Added email into registeration linked login/register
    login/register are visible and link

--- hello/views.py
+++ hello/views.py
@@ -5,5 +5,10 @@ def home(request):
     return render(request, 'hello/home.html')
 
 
+def about(request):
+    return render(request, 'hello/about.html')
+
+
 def index(request):
     return render(request, 'hello/index.html')
+
--- website/settings.py
+++ website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello.apps.HelloConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+Login_URL = 'login'
+
