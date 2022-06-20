a4e6859adb63911fbc69b903a44fb6526c49b646
Alex Park <jihoonhotmail.com@gmail.com>
2022-06-20 16:31:44 +0100

    Cleaning

--- Website_GPS/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('index/', views.index, name='index'),
-    path('tester/', views.AllGpsData, name='tester'),
-]
