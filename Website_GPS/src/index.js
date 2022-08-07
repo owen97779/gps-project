25ffa02cbbc5b4224f90fb5c21f0e8bc239fddbd
Alex Park <jihoonhotmail.com@gmail.com>
2022-08-07 20:40:12 +0100

    more secret

--- Website_GPS/src/index.js
+++ Website_GPS/src/index.js
@@ -2,21 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "../users/Secret/secret";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCSBU0SC6YF_uHkJXTfEG-JEb9BuD94REk",
-  authDomain: "we-jam-ca4be.firebaseapp.com",
-  databaseURL: "https://we-jam-ca4be-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "we-jam-ca4be",
-  storageBucket: "we-jam-ca4be.appspot.com",
-  messagingSenderId: "607588280086",
-  appId: "1:607588280086:web:1a5ad3d88859fef70c9237",
-  measurementId: "G-SRZFTNP7YS"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
